b97a0339ffb4c23fed50ea47eceb54f764285ac4
StaphanieKing <staphaniek@yahoo.com>
2022-10-01 17:35:07 +0000

    finish project

--- src/app.js
+++ src/app.js
@@ -7,5 +7,28 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+  document.querySelector(".excuse").innerHTML = generateExcuse();
   console.log("Hello Rigo from the console!");
+};
+
+let generateExcuse = function() {
+  let who = ["A Cat", "My bird", "His snake", "My dog"];
+  let action = ["stole my", "wet my", "bite my", "broke my"];
+  let what = ["car", "rug", "cookie", "key"];
+  let when = ["today", "yesterday", "last week", "monday"];
+
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let actionIndex = Math.floor(Math.random() * action.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+
+  return (
+    who[whoIndex] +
+    " " +
+    action[actionIndex] +
+    " " +
+    what[whatIndex] +
+    " " +
+    when[whenIndex]
+  );
 };
